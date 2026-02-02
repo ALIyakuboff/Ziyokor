ec2fa1774526909d53badceabc99b31c33664b0d
ALIyakuboff <asdovasd446@gmail.com>
2026-02-02 17:13:09 +0500

    fix(vercel): standard monorepo config - build to apps/web-portal/dist

--- apps/web-portal/vite.config.ts
+++ apps/web-portal/vite.config.ts
@@ -11,10 +11,6 @@ export default defineConfig({
                 changeOrigin: true
             }
         }
-    },
-    build: {
-        outDir: "../../dist",
-        emptyOutDir: true
     }
 
 });
